c6f8f23a531e20fe954105bc78012d6fd13a7263
Wilowayne De La Cruz G <wilo0087@gmail.com>
2019-10-10 11:27:12 -0400

    Removing statics files segment

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const express = require('express')
 const app = express();
 const port = process.env.SERVER_PORT || 3000;
 
-app.use(express.static('public'));
-
 app.get('**', (req, res) => {
 
   for (i= 0; i <= 1000000; i++){
     total = i
   }
+
   response = {msg: `Total de iteraciones: ${total}`}
   res.json(response)
 });
